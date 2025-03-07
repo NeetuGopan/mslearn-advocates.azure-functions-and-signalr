e8a508c63c254386a70eb857b172da34ad96d11c
Kannan Subhash <kannan.subhash@3shape.com>
2025-03-07 12:22:22 +0100

    Update deployment workflow to use package path

--- start/server/src/functions/signalr-open-connection.ts
+++ start/server/src/functions/signalr-open-connection.ts
@@ -4,7 +4,7 @@ const inputSignalR = input.generic({
     type: 'signalRConnectionInfo',
     name: 'connectionInfo',
     hubName: 'default',
-    connectionStringSetting: 'SIGNALR_CONNECTION_STRING',
+    connectionStringSetting: 'Endpoint=https://msl-sigr-signalr8662c1e515.service.signalr.net;AccessKey=7Oy6OgxTGdIN5jesoYswBHEzZnw3Eh4HjavdkkiOgjgmB48SXH8DJQQJ99BCACHYHv6XJ3w3AAAAASRS3L3e;Version=1.0;',
 });
 
 app.http('open-signalr-connection', {
